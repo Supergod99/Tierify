60b96e9ed731c3da98ec6846e8818bbb26fdffd1
Supergod99 <rsupergod99@gmail.com>
2025-12-05 04:36:54 -0500

    Update TooltipOverhaulTextMixin.java

--- src/main/java/elocindev/tierify/mixin/compat/TooltipOverhaulTextMixin.java
+++ src/main/java/elocindev/tierify/mixin/compat/TooltipOverhaulTextMixin.java
@@ -4,16 +4,16 @@ import dev.xylonity.tooltipoverhaul.client.style.text.DefaultText;
 import elocindev.tierify.screen.client.component.PerfectTierComponent;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
+
+import net.minecraft.util.math.Vec2f; 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.awt.Point;
-import java.util.List;
 
 @Mixin(DefaultText.class)
 public class TooltipOverhaulTextMixin {
-
 
     @Redirect(
         method = "render",
@@ -22,22 +22,29 @@ public class TooltipOverhaulTextMixin {
             target = "Lnet/minecraft/client/gui/tooltip/TooltipComponent;drawText(Lnet/minecraft/client/font/TextRenderer;IILorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;)V"
         )
     )
-    private void tierify$centerPerfectLabel(TooltipComponent instance, TextRenderer textRenderer, int x, int y, org.joml.Matrix4f matrix, net.minecraft.client.render.VertexConsumerProvider.Immediate vertexConsumers, 
+    private void tierify$centerPerfectLabel(TooltipComponent instance, 
+                                            TextRenderer textRenderer, 
+                                            int x, int y, 
+                                            org.joml.Matrix4f matrix, 
+                                            net.minecraft.client.render.VertexConsumerProvider.Immediate vertexConsumers, 
+
                                             dev.xylonity.tooltipoverhaul.client.layer.LayerDepth depth, 
                                             dev.xylonity.tooltipoverhaul.client.TooltipContext ctx, 
-                                            net.minecraft.class_241 pos, 
-                                            Point size) {
+                                            Vec2f pos, 
+                                            Point size) { 
         
         int drawX = x;
         
         if (instance instanceof PerfectTierComponent) {
 
+            
             int tooltipWidth = size.x;
             int componentWidth = instance.getWidth(textRenderer);
             
 
+            int absoluteLeft = (int) pos.x;
             
-            int absoluteLeft = (int) pos.field_1343;
+
             drawX = absoluteLeft + (tooltipWidth - componentWidth) / 2;
         }
 
