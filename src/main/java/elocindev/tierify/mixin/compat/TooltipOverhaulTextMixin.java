30b11010ffef7aa0975b203842715c0560c06215
Supergod99 <rsupergod99@gmail.com>
2025-12-05 22:03:22 -0500

    Update TooltipOverhaulTextMixin.java

--- src/main/java/elocindev/tierify/mixin/compat/TooltipOverhaulTextMixin.java
+++ src/main/java/elocindev/tierify/mixin/compat/TooltipOverhaulTextMixin.java
@@ -4,7 +4,9 @@ import dev.xylonity.tooltipoverhaul.client.style.text.DefaultText;
 import dev.xylonity.tooltipoverhaul.client.TooltipContext;
 import dev.xylonity.tooltipoverhaul.client.layer.LayerDepth;
 import dev.xylonity.tooltipoverhaul.util.Util;
+import elocindev.tierify.screen.client.component.PerfectTierComponent;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.util.math.Vec2f;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,42 +18,47 @@ import java.awt.Point;
 @Mixin(DefaultText.class)
 public class TooltipOverhaulTextMixin {
 
-
+    // Strategy: Intercept the X-coordinate calculation.
+    // The target method signature is derived from your compile error log:
+    // (int, int, Point, TooltipComponent, TextRenderer, TooltipContext)
     @Redirect(
         method = "render(Ldev/xylonity/tooltipoverhaul/client/layer/LayerDepth;Ldev/xylonity/tooltipoverhaul/client/TooltipContext;Lnet/minecraft/util/math/Vec2f;Ljava/awt/Point;Lnet/minecraft/text/Text;Lnet/minecraft/client/font/TextRenderer;)V",
         at = @At(
             value = "INVOKE",
-            target = "Ldev/xylonity/tooltipoverhaul/util/Util;getTitleAlignmentX(Ldev/xylonity/tooltipoverhaul/client/TooltipContext;FF)I"
+            target = "Ldev/xylonity/tooltipoverhaul/util/Util;getTitleAlignmentX(IILjava/awt/Point;Lnet/minecraft/client/gui/tooltip/TooltipComponent;Lnet/minecraft/client/font/TextRenderer;Ldev/xylonity/tooltipoverhaul/client/TooltipContext;)I"
         )
     )
     private int tierify$modifyTitleAlignment(
-            TooltipContext ctx, 
-            float containerWidth, 
-            float textWidth, 
-
+            // Arguments passed to Util.getTitleAlignmentX
+            int x, 
+            int y, 
+            Point containerSize, 
+            TooltipComponent component, 
+            TextRenderer fontRenderer, 
+            TooltipContext context,
+            
+            // Captured locals from the render method (we need 'pos' for absolute coordinates)
             LayerDepth depth,
-            TooltipContext ctx2,
+            TooltipContext capturedCtx,
             Vec2f pos,
-            Point size,
+            Point capturedSize,
             Text rarity,
-            TextRenderer font
+            TextRenderer capturedFont
     ) {
+        // 1. Calculate default behavior
+        int originalX = Util.getTitleAlignmentX(x, y, containerSize, component, fontRenderer, context);
 
-        int originalX = Util.getTitleAlignmentX(ctx, containerWidth, textWidth);
-
- 
-        if (rarity == null) {
-            return originalX;
-        }
-
-
-        if (rarity.getString().contains("Perfect")) {
+        // 2. Check if this is the "Perfect" component
+        if (component instanceof PerfectTierComponent) {
             
-
+            // 3. Force Center Alignment
+            // Logic: Absolute Left Edge + (Container Width - Component Width) / 2
             int absoluteLeft = (int) pos.x;
-            int centeredX = absoluteLeft + (int)((containerWidth - textWidth) / 2.0f);
+            int containerWidth = containerSize.x;
+            int componentWidth = component.getWidth(fontRenderer);
             
-            return centeredX;
+            // Recalculate center
+            return absoluteLeft + (containerWidth - componentWidth) / 2;
         }
 
         return originalX;
