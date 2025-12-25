f2d09effa8f9a1154a519ea3a4aafa3cbf889f6c
Supergod99 <rsupergod99@gmail.com>
2025-12-24 21:46:17 -0500

    Update ItemRegistry.java

--- src/main/java/elocindev/tierify/registry/ItemRegistry.java
+++ src/main/java/elocindev/tierify/registry/ItemRegistry.java
@@ -11,6 +11,7 @@ public class ItemRegistry {
     public static final Item LIMESTONE_CHUNK = register(new ReforgeAddition(new Item.Settings(), 1), "limestone_chunk");
     public static final Item RAW_PYRITE = register(new ReforgeAddition(new Item.Settings(), 2), "pyrite_chunk");
     public static final Item RAW_GALENA = register(new ReforgeAddition(new Item.Settings(), 3), "galena_chunk");
+    public static final Item PAINITE_CHUNK = register(new ReforgeAddition(new Item.Settings(), 4), "painite_chunk");
     public static final Item CLEANSING_STONE = register(new ReforgeAddition(new Item.Settings(), 0), "cleansing_stone");
 
     public static void init() {}
