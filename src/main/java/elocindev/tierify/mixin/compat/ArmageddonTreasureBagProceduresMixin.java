9d8ffee0056045de5c7793e04b6738c25ae83db8
Supergod99 <rsupergod99@gmail.com>
2025-12-31 15:45:25 -0500

    Update ArmageddonTreasureBagProceduresMixin.java

--- src/main/java/elocindev/tierify/mixin/compat/ArmageddonTreasureBagProceduresMixin.java
+++ src/main/java/elocindev/tierify/mixin/compat/ArmageddonTreasureBagProceduresMixin.java
@@ -1,43 +1,48 @@
 package elocindev.tierify.mixin.compat;
 
 import elocindev.tierify.compat.ArmageddonTreasureBagHooks;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Pseudo
 @Mixin(
-    targets = {
-        "net.mcreator.armageddonmod.procedures.ArionTreasurebagRightclickedProcedure",
-        "net.mcreator.armageddonmod.procedures.IronColossusTreasureBagRightclickedProcedure"
-    },
-    remap = false
+        targets = {
+                "net.mcreator.armageddonmod.procedures.ArionTreasurebagRightclickedProcedure",
+                "net.mcreator.armageddonmod.procedures.IronColossusTreasureBagRightclickedProcedure"
+        },
+        remap = false
 )
-public class ArmageddonTreasureBagProceduresMixin {
+public abstract class ArmageddonTreasureBagProceduresMixin {
+
+    /**
+     * Armageddon procedures spawn loot with: new ItemEntity(_level, x, y, z, new ItemStack(...))
+     * We rewrite the ItemStack argument (index 4) to a possibly-reforged stack.
+     *
+     * NOTE: remap=true here is important under Connector so the ItemEntity ctor target can be matched.
+     */
     @ModifyArg(
-        method = "execute",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/entity/ItemEntity;<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/item/ItemStack;)V"
-        ),
-        index = 4,
-        require = 0
+            method = "execute(Lnet/minecraft/world/level/LevelAccessor;DDDLnet/minecraft/world/entity/Entity;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/ItemEntity;<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/item/ItemStack;)V"
+            ),
+            index = 4,
+            remap = true
     )
-    private static ItemStack echelon$maybeReforgeSpawnedStack(ItemStack stack,
-                                                             @Coerce Object levelAccessor,
-                                                             double x, double y, double z,
-                                                             @Coerce Object entityObj) {
-        try {
-            Entity yarnEntity = (Entity) entityObj;
-            return ArmageddonTreasureBagHooks.maybeReforgeFromHeldBag(stack, yarnEntity);
-        } catch (Throwable t) {
-            return stack;
+    private static ItemStack echelon$maybeReforgeSpawnedStack(
+            ItemStack stack,
+            Object world,
+            double x,
+            double y,
+            double z,
+            Object entity
+    ) {
+        if (entity instanceof net.minecraft.entity.Entity e) {
+            return ArmageddonTreasureBagHooks.maybeReforgeFromHeldBag(stack, e);
         }
+        return stack;
     }
 }
