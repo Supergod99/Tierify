b083854fab5dc1eb7d78cb119d0032e74e2fd43d
Supergod99 <rsupergod99@gmail.com>
2025-12-31 11:53:36 -0500

    Update ArmageddonTreasureBagProceduresMixin.java

--- src/main/java/elocindev/tierify/mixin/compat/ArmageddonTreasureBagProceduresMixin.java
+++ src/main/java/elocindev/tierify/mixin/compat/ArmageddonTreasureBagProceduresMixin.java
@@ -2,11 +2,13 @@ package elocindev.tierify.mixin.compat;
 
 import elocindev.tierify.compat.ArmageddonTreasureBagHooks;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Pseudo
@@ -14,30 +16,28 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
     targets = {
         "net.mcreator.armageddonmod.procedures.ArionTreasurebagRightclickedProcedure",
         "net.mcreator.armageddonmod.procedures.IronColossusTreasureBagRightclickedProcedure"
-        // Add more procedure class names here as Armageddon adds bags (still ONE mixin file).
     },
     remap = false
 )
 public class ArmageddonTreasureBagProceduresMixin {
-
     @ModifyArg(
-        method = "execute(Lnet/minecraft/world/level/LevelAccessor;DDDLnet/minecraft/world/entity/Entity;)V",
+        method = "execute",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/item/ItemEntity;<init>(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V"
+            target = "Lnet/minecraft/entity/ItemEntity;<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/item/ItemStack;)V"
         ),
         index = 4,
-        require = 0,
-        remap = false
+        require = 0
     )
-    private static net.minecraft.world.item.ItemStack echelon$maybeReforgeSpawnedStack(
-        net.minecraft.world.item.ItemStack stack,
-        net.minecraft.world.level.LevelAccessor world,
-        double x, double y, double z,
-        net.minecraft.world.entity.Entity entity
-    ) {
-        // Armageddon is Mojmap/Forge; use their types here and keep remap=false.
-        return (net.minecraft.world.item.ItemStack)(Object)
-            ArmageddonTreasureBagHooks.maybeReforgeFromHeldBag((ItemStack)(Object)stack, (Entity)(Object)entity);
+    private static ItemStack echelon$maybeReforgeSpawnedStack(ItemStack stack,
+                                                             @Coerce Object levelAccessor,
+                                                             double x, double y, double z,
+                                                             @Coerce Object entityObj) {
+        try {
+            Entity yarnEntity = (Entity) entityObj;
+            return ArmageddonTreasureBagHooks.maybeReforgeFromHeldBag(stack, yarnEntity);
+        } catch (Throwable t) {
+            return stack;
+        }
     }
 }
