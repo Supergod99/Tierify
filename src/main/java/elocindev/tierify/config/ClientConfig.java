45dd0bef10b40adfdb8ec9212e78395bef77115d
Supergod99 <rsupergod99@gmail.com>
2026-01-06 13:10:55 -0500

    Update ClientConfig.java

--- src/main/java/elocindev/tierify/config/ClientConfig.java
+++ src/main/java/elocindev/tierify/config/ClientConfig.java
@@ -28,4 +28,18 @@ public class ClientConfig implements ConfigData {
 
     @ConfigEntry.Category("client_settings")
     public boolean centerName = true;
+
+    @@
+     @ConfigEntry.Category("client_settings")
+     public boolean centerName = true;
+
+    @Comment("TooltipOverhaul specialEffect override for Tierify tiered tooltips. Leave blank to use Tierify defaults. " +
+            "You can stack multiple effects separated by ',' or ';' (e.g. \"galaxy, white_dust\").")
+    @ConfigEntry.Category("client_settings")
+    public String ttoSpecialEffectOverride = "";
+
+    @Comment("If true (and override is blank), Tierify assigns a TooltipOverhaul effect per border template index for quick testing.")
+    @ConfigEntry.Category("client_settings")
+    public boolean ttoSpecialEffectByTemplateIndex = false;
+ 
 }
